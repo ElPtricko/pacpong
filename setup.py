cbfc3607fea6b8a4b7cfb25339bdd4200b2894c7
ElPtricko <elptricko@gmail.com>
2019-03-11 21:36:34 +0100

    Changed setup file as tkinter wasn't needed.

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 from cx_Freeze import setup, Executable
 import sys
-import os.path
+# import os.path
 
 build_exe_options = {"packages": ["os", "cocos", "pyglet", "win32api",
                                   "random"],
                      "includes": ["pyglet.resource"],
-                     "include_files": ["tcl86t.dll", "tk86t.dll",
-                                       "resources", "glvars.py"]}
+                     "include_files": [# "tcl86t.dll", "tk86t.dll",
+                                       "resources", "glvars.py"],
+                     "excludes": ["tkinter"]}
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+# PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+# os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+# os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
 
-setup(options = {"build_exe": build_exe_options},
+setup(options={"build_exe": build_exe_options},
       name='PacPong',
-      version = '0.1',
+      version='0.1',
       description='PacPong',
       executables=[Executable("Game.py", base=base)])
