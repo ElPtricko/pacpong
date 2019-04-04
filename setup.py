fd9f225576ee42210349d621071d68eca2438356
ElPtricko <elptricko@gmail.com>
2019-04-04 14:12:53 +0200

    Updated setup file.

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 from cx_Freeze import setup, Executable
-import sys
-# import os.path
+import sys, platform
 
-build_exe_options = {"packages": ["os", "cocos", "pyglet", "random"
-                                  # "AppKit", "pkg_resources._vendor", "_sysconfigdata_m_darwin_darwin",
-                                  ],
-                     "includes": ["pyglet.resource"],
-                     "include_files": [# "tcl86t.dll", "tk86t.dll",
-                                       "resources", "glvars.py"],
-                     "excludes": ["tkinter"]}
+if platform.system() == 'Windows':
+    build_exe_options = {"packages": ["os", "cocos", "pyglet", "random", "win32api"],
+                         "includes": ["pyglet.resource"],
+                         "include_files": ["resources", "glvars.py", "Game.py", "progressBar.py", "sprites.py"],
+                         "excludes": ["tkinter"]}
+elif platform.system() == 'Darwin':
+    build_exe_options = {"packages": ["os", "cocos", "pyglet", "random",
+                                      "AppKit", "pkg_resources._vendor", "_sysconfigdata_m_darwin_darwin",
+                                      ],
+                         "includes": ["pyglet.resource"],
+                         "include_files": ["resources", "glvars.py", "Game.py", "progressBar.py", "sprites.py"],
+                         "excludes": ["tkinter"]}
+
+else:
+    raise OSError('Your platform is not supported!')
 
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
 
-# PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
-# os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
-# os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
-
-
 setup(options={"build_exe": build_exe_options},
       name='PacPong',
       version='0.1',
       description='PacPong',
-      executables=[Executable("Game.py", base=base)])
+      executables=[Executable("main.py", base=base)])
